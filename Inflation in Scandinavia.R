1e2e3241ad6a9c47eeefcf9c4a19c6a86db0e33a
Mohamad Nasher <100182548+Mohamad-Nasher@users.noreply.github.com>
2022-06-18 00:53:36 +0300

    Update Inflation in Scandinavia.R

--- Inflation in Scandinavia.R
+++ Inflation in Scandinavia.R
@@ -30,4 +30,16 @@ ggplot(data = data1 , mapping = aes(x = year , y = rate , color = country)) +
         ,axis.title.y = element_text(family = "Rubik" , face = "bold")) + 
   scale_x_continuous(breaks = 2008:2019) + 
   scale_color_manual(values = c("#82E0AA","#002F6C","#BA0C2F","#FECC02")) + 
-  annotate("curve" , x = 2013.2 , y = 0.15 , xend = 2014.8 , yend = -0.2 ,color ="darkgrey", curvature = 0.3 , arrow = arrow(length = unit(2 , "mm"))) + annotate("text" , x = 2012.25 , y = 0.17 , label = ("Finland Experiences \n -0.2 Deflation") , family = "Rubik" , color = "darkgrey" , fontface = "bold" , size = 4)
+  annotate("curve" 
+           ,x = 2013.2 
+           ,y = 0.15 
+           , xend = 2014.8 
+           , yend = -0.2 
+           ,color ="darkgrey"
+           , curvature = 0.3 
+           , arrow = arrow(length = unit(2 , "mm"))) +
+ annotate("text" 
+          , x = 2012.25 
+          , y = 0.17 
+          , label = ("Finland Experiences \n -0.2 Deflation") 
+          , family = "Rubik" , color = "darkgrey" , fontface = "bold" , size = 4)
