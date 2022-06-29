a0ed01c3d218ec987a506f4dddb15a77234c9dd1
Mohamad Nasher <100182548+Mohamad-Nasher@users.noreply.github.com>
2022-06-30 01:09:51 +0300

    Update Inflation in Scandinavia.R

--- Inflation in Scandinavia/Inflation in Scandinavia.R
+++ Inflation in Scandinavia/Inflation in Scandinavia.R
@@ -1,4 +1,17 @@
-ggplot(data = data1 , mapping = aes(x = year , y = rate , color = Country)) +
+# libs 
+# install.packages("tidyverse")
+# install.packages("ggthemes")
+
+# Importing the Data
+data1 <- read.csv("europe inflation.csv")
+
+# Cleaning the Data
+data1 <- data1 %>% filter(country == "Norway" | country =="Sweden" |country == "Denmark" | country == "Finland")
+ 
+ 
+
+#plot
+ggplot(data = data1 , mapping = aes(x = year , y = rate , color = country)) +
   geom_line(size = 2) + geom_point(size = 3) + 
   labs(title = "A Decade of Scandinavian Inflation" 
        , x = "Year" 
@@ -17,4 +30,16 @@ ggplot(data = data1 , mapping = aes(x = year , y = rate , color = Country)) +
         ,axis.title.y = element_text(family = "Rubik" , face = "bold")) + 
   scale_x_continuous(breaks = 2008:2019) + 
   scale_color_manual(values = c("#82E0AA","#002F6C","#BA0C2F","#FECC02")) + 
-  annotate("curve" , x = 2013.2 , y = 0.15 , xend = 2014.8 , yend = -0.2 ,color ="darkgrey", curvature = 0.3 , arrow = arrow(length = unit(2 , "mm"))) + annotate("text" , x = 2012.25 , y = 0.17 , label = ("Finland Experiences \n -0.2 Deflation") , family = "Rubik" , color = "darkgrey" , fontface = "bold" , size = 4)
+  annotate("curve" 
+           ,x = 2013.2 
+           ,y = 0.15 
+           , xend = 2014.8 
+           , yend = -0.2 
+           ,color ="darkgrey"
+           , curvature = 0.3 
+           , arrow = arrow(length = unit(2 , "mm"))) +
+ annotate("text" 
+          , x = 2012.25 
+          , y = 0.17 
+          , label = ("Finland Experiences \n -0.2 Deflation") 
+          , family = "Rubik" , color = "darkgrey" , fontface = "bold" , size = 4)
