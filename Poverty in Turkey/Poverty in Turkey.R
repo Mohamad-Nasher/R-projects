64d1df67d15a4d20ebcf4f414ea329dd4f485bd7
Mohamad Nasher <100182548+Mohamad-Nasher@users.noreply.github.com>
2022-06-30 01:01:42 +0300

    Update Poverty in Turkey.R

--- Poverty in Turkey/Poverty in Turkey.R
+++ Poverty in Turkey/Poverty in Turkey.R
@@ -1,6 +1,27 @@
+## libs 
+## install.packages("tidyverse")
+## install.packages("sf")
+## install.packages("extrafont")
+## instll.packages("devtools")
+## devtools::install_github("htastan/TRmaps")
+
+## Importing the Data 
+map <- tr_nuts2
+data <- read.xlsx("poverty.xlsx")
+
+## Cleaning the Data 
+data <- data %>% drop_na() %>% mutate(code = str_sub(regioin , start = 1 , end = 4))
+map <- map %>% rename(code = NUTS2_code)
+
+## Merge
+df <- innerjoin(map , data , by = "code)
+
+## plot
 ggplot() + geom_sf(data = df ,aes(fill = poverty_rate)) +
   scale_fill_gradientn(colours = c("#59CD90" , "#3FA7D6", "#EE6352")) +
-  labs(title = "Poverty Rates in Turkey " , caption = "Data Viz By :@AnalystNasher | Data Source :TÜRKSTAT" , fill = "Poverty %") +
+  labs(title = "Poverty Rates in Turkey " ,
+       caption = "Data Viz By :@AnalystNasher | Data Source :TÜRKSTAT" ,
+       fill = "Poverty %") +
   theme(panel.grid = element_blank() ,
         panel.background = element_blank() ,
         plot.title = element_text(size = 25 ,
@@ -24,3 +45,4 @@ ggplot() + geom_sf(data = df ,aes(fill = poverty_rate)) +
                                     colour = "#454955" ,
                                     face = "bold" ,
                                     family = "Palatino Linotype"))
+
